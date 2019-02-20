c7edd37f5387857c9a78daae133c41816b5b3039
Evgeny Batalov <eugene_batalov@mail.ru>
2019-02-20 13:29:39 +0300

    build

---
+++ tmp/build.ps1
@@ -0,0 +1,105 @@
+cd CS_CommonTypes
+mvn clean install
+cd ..
+cd CS_Configurator
+mvn clean install
+cd ..
+cd CS_CacheClient
+mvn clean install
+cd ..
+cd CS_DbIntf
+mvn clean install
+cd ..
+cd CS_Tools
+mvn clean install
+cd ..
+cd SignServiceClient
+mvn clean install
+cd ..
+cd CS_CryptoService
+mvn clean install
+cd ..
+cd CS_ArchiveClient
+mvn clean install
+cd ..
+cd CS_ArchiveCallbackClient
+mvn clean install
+cd ..
+cd CS_Billing
+mvn clean install
+cd ..
+cd CS_BillingCallbackClient
+mvn clean install
+cd ..
+cd CS_BillingClient
+mvn clean install
+cd ..
+cd CS_CaAdapterApi
+mvn clean install
+cd ..
+cd CS_CallbackClient
+mvn clean install
+cd ..
+cd CS_ForisClient
+mvn clean install
+cd ..
+cd CS_SFTPSUtils
+mvn clean install
+cd ..
+cd CS_MtsArchiveClient
+mvn clean install
+cd ..
+cd CS_PartnerAccount
+mvn clean install
+cd ..
+cd CS_CacheServer
+mvn clean install
+cd ..
+cd CS_PartnerService
+mvn clean install
+cd ..
+cd CS_ExpiredSignatureDetector
+mvn clean install
+cd ..
+cd CS_ForisService
+mvn clean install
+cd ..
+cd CS_MtsArchiveService
+mvn clean install
+cd ..
+cd CS_SimCardLoader
+mvn clean install
+cd ..
+cd CS_ValimoClient
+mvn clean install
+cd ..
+cd CS_CryptoProDSS
+mvn clean install
+cd ..
+cd CS_RaServiceProxy
+mvn clean install
+cd ..
+cd CS_CPCASoapEmulator
+mvn clean install
+cd ..
+cd CS_DirectorsBoardMTS
+mvn clean install
+cd ..
+cd CS_EasySign
+mvn clean install
+cd ..
+cd CS_ExternalSystemStubService
+mvn clean install
+cd ..
+cd CS_MTSDemoCryptoSim
+mvn clean install
+cd ..
+cd CS_PartnerCallbackService
+mvn clean install
+cd ..
+cd CS_RaServiceEmulator
+mvn clean install
+cd ..
+cd CS_SignServiceClient
+mvn clean install
+cd ..
